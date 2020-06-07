bf3ad389d0ec5a4c3dee166a58093574e5dcc2ea
angieappiah <alegnalawn2@yahoo.com>
2020-06-06 23:26:23 -0700

    bibini-project

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,10 +2,6 @@ class User < ActiveRecord::Base
     has_secure_password
     has_many :products
 
-    validates :username, presence: true, uniqueness: true, length: { minimum: 4 }, format: { with: /\A[a-zA-Z]+\z/, message: "cannot contain blank space, numbers, or special characters" }
-    validates :email, presence: true, format: { with: /[^@]+@[^\.]+\..+/, message: "must be in format xxxx@xxxx.xxx"}
-    validates :password, presence: true, length: { in: 6..20 }
-
     def slug
         self.username.gsub(" ", "-").downcase
     end
