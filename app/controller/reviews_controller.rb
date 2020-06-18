ba3cb945680d2494e6f0ef909d66d54719eb465f
angieappiah <alegnalawn2@yahoo.com>
2020-06-18 06:29:47 -0700

    bibini-products

--- app/controller/reviews_controller.rb
+++ app/controller/reviews_controller.rb
@@ -1,13 +1,30 @@
 class ReviewsController < ApplicationController
 
   get '/reviews' do
-    
-    erb :"/reviews/show_review"
+    @user = current_user
+    @products = Product.all
+    erb :"/reviews/products"
   end
 
   get '/reviews:new' do
     @products = Product.new
     erb :"/reviews/new_review"
+  end
+
+  post '/reviews' do
+    @products = Product.all
+    erb :"/reviews/show_review"
+  end
+
+  patch '/reviews' do
+    @product = Product.find_by(id: params[:id])
+    @product.update(title: params[:title], description: params[:description], price: params[:price], image: params[:image])
+            
+    if @product.errors.any?
+      erb :"/reviews/edit_review"
+    else
+      erb :"reviews/show_review"
+    end
   end
 end
 
