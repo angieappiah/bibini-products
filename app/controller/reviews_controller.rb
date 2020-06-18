8b6dcaabda502b23c33619b7b96ed234dbf4e887
angieappiah <alegnalawn2@yahoo.com>
2020-06-18 05:34:22 -0700

    bibini-products

--- app/controller/reviews_controller.rb
+++ app/controller/reviews_controller.rb
@@ -1,29 +1,24 @@
 class ReviewsController < ApplicationController
 
-    get '/reviews' do
-      @user = current_user
-      @products = Product.all
-      @reviews = Review.all
-      erb :"reviews/products"
-    end
-
-    get '/reviews/:id' do  
-      @product = Product.find_by_id(params[:id])
-      erb :"reviews/show_product"
-    end
-
-    get '/reviews/:id/edit' do
-      @product = Product.find_by(id: params[:id])
-  
-      if logged_in? && @product.user_id == current_user.id
-        erb :"reviews/edit_product"
-      else
-        @failed = true
-        erb :"reviews/show_product"
-      end
+  get '/reviews' do
+    
+    erb :"/reviews/show_review"
   end
 
+  get '/reviews:new' do
+    @products = Product.new
+    erb :"/reviews/new_review"
+  end
+end
+
+
+   
+  
+
+
+
+  
+
    
 
    
-end
